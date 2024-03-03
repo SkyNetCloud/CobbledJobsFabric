e7460d13b0b303ab1b6bf13edf2fc38a43ac097b
SkyNetCloud <mcboxer1201@gmail.com>
2024-03-03 11:29:26 -0500

    1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("java")
-    id("dev.architectury.loom") version("0.12.0-SNAPSHOT")
+    id("dev.architectury.loom") version("1.0-SNAPSHOT")
     id("architectury-plugin") version("3.4-SNAPSHOT")
     kotlin("jvm") version ("1.7.10")
 }
 
 group = "io.github.adainish"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 architectury {
     platformSetupLoomIde()
@@ -25,19 +25,26 @@ repositories {
     mavenCentral()
     maven(url = "https://dl.cloudsmith.io/public/geckolib3/geckolib/maven/")
     maven("https://maven.impactdev.net/repository/development/")
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        name = "sonatype-oss-snapshots1"
+        mavenContent { snapshotsOnly() }
+    }
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.2")
+    minecraft("com.mojang:minecraft:1.20.1")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.14.14")
+    modImplementation("net.fabricmc:fabric-loader:0.15.6")
 
-    modImplementation(fabricApi.module("fabric-command-api-v2", "0.75.1+1.19.2"))
-    modImplementation("dev.architectury", "architectury-fabric", "6.5.69")
-    modImplementation("com.cobblemon:fabric:1.3.1+1.19.2-SNAPSHOT")
+    //modImplementation(fabricApi.module("fabric-command-api-v2", "0.92.0+1.19.2"))
+    modImplementation("dev.architectury", "architectury-fabric", "9.2.14")
 
-    modImplementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation(fileTree(mapOf("dir" to "impactor", "include" to listOf("*.jar"))))
+    modImplementation("ca.landonjw.gooeylibs:api:3.0.0-1.20.1-SNAPSHOT")
+    modImplementation("com.cobblemon:mod:1.4.1+1.20.1-SNAPSHOT")
+    implementation("net.impactdev.impactor.api:economy:5.1.1-SNAPSHOT")
+    implementation("net.impactdev.impactor.api:core:5.1.1-SNAPSHOT")
+
+    implementation("net.kyori:adventure-api:4.14.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
